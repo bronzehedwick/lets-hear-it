65907e11d44b87e71f0ae18ff51f23724c93e90a
Chris DeLuca <code@chrisdeluca.me>
2020-09-27 16:11:42 -0400

    Change author to me
    Let's have some ego.

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'lets-hear-it' ), 'lets-hear-it', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'lets-hear-it' ), 'lets-hear-it', '<a href="http://www.chrisdeluca.me/">Chris DeLuca</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
