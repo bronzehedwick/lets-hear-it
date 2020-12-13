70da2c3e1b067663c2fc78f73ceb22a836c19a53
Chris DeLuca <code@chrisdeluca.me>
2020-12-13 17:34:17 -0500

    Add date-based cache busting
    The built in WP cache busting uses version numbers, which is too manual
    and brittle. Use date modified as a performance compromise for a hash of
    the file contents.

--- functions.php
+++ functions.php
@@ -465,6 +465,55 @@ function lhi_breadcrumb() {
 }
 
 /**
+ * WordPress Cache Busting made simple.
+ *
+ * @author Recolize GmbH <service@recolize.com>
+ * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License Version 3 (GPLv3)
+ *
+ * This script is based on
+ * @see https://medium.com/@futuremediagr/easy-versioning-for-css-and-js-files-in-wordpress-e7dad756586c
+ * @see https://gist.github.com/ocean90/1966227
+ */
+function lhi_set_custom_ver_css_js($src) {
+	// Don't touch admin scripts.
+	if (is_admin()) {
+		return $src;
+	}
+
+	$_src = $src;
+	if (strpos($_src, '//') === 0) {
+		$_src = 'https:' . $_src;
+	}
+
+	$_src = parse_url($_src);
+
+	// Give up if malformed URL.
+	if (false === $_src) {
+		return $src;
+	}
+
+	// Check if it's a local URL.
+	$wordPressUrl = parse_url(home_url());
+	if (isset($_src['host']) && $_src['host'] !== $wordPressUrl['host']) {
+		return $src;
+	}
+
+	$filePath = ABSPATH . $_src['path'];
+	if (file_exists($filePath) && strpos($src, 'ver=') !== false) {
+		$src = add_query_arg('ver', filemtime($filePath), $src);
+	}
+
+	return $src;
+}
+
+function lhi_css_js_versioning() {
+	add_filter('style_loader_src', 'lhi_set_custom_ver_css_js', 9999);
+	add_filter('script_loader_src', 'lhi_set_custom_ver_css_js', 9999);
+}
+
+add_action('init', 'lhi_css_js_versioning');
+
+/**
  * Enqueue scripts and styles.
  */
 function lets_hear_it_scripts() {
