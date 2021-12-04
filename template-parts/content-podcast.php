842362884cbc60102b3c158f39cd5368c3f1a84a
Chris DeLuca <code@chrisdeluca.me>
2021-12-04 16:23:05 -0500

    Hide vim comments from public display

--- template-parts/content-podcast.php
+++ template-parts/content-podcast.php
@@ -61,7 +61,6 @@
 		<?php lets_hear_it_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
-
-<!--
-vim: tabstop=4 shiftwidth=4 noexpandtab nolist
--->
+<?php
+// vim: tabstop=4 shiftwidth=4 noexpandtab nolist
+?>
